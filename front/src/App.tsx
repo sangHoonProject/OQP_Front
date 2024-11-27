e63c16fcd81bbf12bb8e137c824c051dcbda9b16
kingkaminseo <rlazmfdl@gmail.com>
2024-11-27 10:14:14 +0900

    🎉 프로젝트 갈아엎고 지금부터 시작

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,25 +1,8 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
-import Main from './views/Main';
-import Signup from './Sign/Signup';
-import Signin from './Sign/Signin';
-import Profil from './views/Profil';
-import ForgotPassWord from './ForgotPassword';
-import Write from './views/Write';
 
 function App() {
-	return (
-		<BrowserRouter>
-			<Routes>
-				<Route path="/" element={<Main />} />
-				<Route path="/signup" element={<Signup />} />
-				<Route path="/signin" element={<Signin />} />
-				<Route path="/Profil" element={<Profil />} />
-				<Route path="/forgotpassword" element={<ForgotPassWord />} />
-				<Route path="/write" element={<Write />} />
-			</Routes>
-		</BrowserRouter>
-	);
+	return <></>;
 }
 
 export default App;
